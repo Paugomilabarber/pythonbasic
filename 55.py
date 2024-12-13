a3effd131e0a4211c4f4a39b1cb3a133425ef91e
Paugomilabarber <pgomila20959@iesjoanramis.org>
2024-12-13 15:39:28 +0100

    as

--- 55.py
+++ 55.py
@@ -1,10 +1,17 @@
-
-
+"""
+Escriure un programa que sumi tots els números entre dos números donats, ambdós inclosos.
+x=int(input("Escriu el menor: "))
+y=int(input("Escriu el major: "))
+suma=0
+for i in range(x,y+1,1):
+    suma+=i
+print(suma)
+"""
 
 x=int(input("Escriu el menor: "))
 y=int(input("Escriu el major: "))
 suma=0
 for i in range(x,y+1,1):
     if i%2==0:
-        suma+=1
+        suma+=i
 print(suma)
