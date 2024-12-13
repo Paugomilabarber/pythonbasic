f95f73abd086ad103be8df7b26b5a992cf567c0e
Paugomilabarber <pgomila20959@iesjoanramis.org>
2024-12-13 11:58:22 +0100

    llegir

--- 51.py
+++ 51.py
@@ -1,3 +1,11 @@
-with open("PROVA1.txt","w") as f:
-    for i in range(10):
-        f.writelines(str(i+1))
+
+def crear_llista_fitxer(nom):
+    l=[]
+    with open(nom,"r") as f:
+        for line in f:
+            l.append(line[:-3])
+        return l
+    
+#programa principal
+l=crear_llista_fitxer("PROVA1.txt")
+print(l)
