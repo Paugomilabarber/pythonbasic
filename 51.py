51d2280ae91418b7ac7ff915092b16d76048d160
Paugomilabarber <pgomila20959@iesjoanramis.org>
2024-12-13 11:45:01 +0100

    3

--- 51.py
+++ 51.py
@@ -1,14 +1,3 @@
-def crear_llista_fitxer(nom_fitxer):
-    try:
-        with open(nom_fitxer, 'r', encoding='utf-8') as fitxer:
-            contingut = fitxer.read()
-            llista_paraules = contingut.split()
-        return llista_paraules
-    except FileNotFoundError:
-        print(f"Error: El fitxer {nom_fitxer} no existeix.")
-        return []
-
-
-nom_fitxer = 'exemple.txt'
-llista_paraules = crear_llista_fitxer(nom_fitxer)
-print("Paraules del fitxer:", llista_paraules)
+with open("PROVA1.txt","w") as f:
+    for i in range(10):
+        f.writelines(str(i+1))
